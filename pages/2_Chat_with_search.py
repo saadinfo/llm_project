aa1c79acbe9655843c2e511b65b9cdb8ee9e21a1
saadinfo <33161347+saadinfo@users.noreply.github.com>
2023-11-11 15:49:11 +0100

    first commit

--- pages/2_Chat_with_search.py
+++ pages/2_Chat_with_search.py
@@ -7,16 +7,9 @@ from langchain.tools import DuckDuckGoSearchRun
 
 with st.sidebar:
     openai_api_key = st.text_input("OpenAI API Key", key="langchain_search_api_key_openai", type="password")
-    "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
-    "[View the source code](https://github.com/streamlit/llm-examples/blob/main/pages/2_Chat_with_search.py)"
-    "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
-
+   
 st.title("🔎 LangChain - Chat with search")
 
-"""
-In this example, we're using `StreamlitCallbackHandler` to display the thoughts and actions of an agent in an interactive Streamlit app.
-Try more LangChain 🤝 Streamlit Agent examples at [github.com/langchain-ai/streamlit-agent](https://github.com/langchain-ai/streamlit-agent).
-"""
 
 if "messages" not in st.session_state:
     st.session_state["messages"] = [
